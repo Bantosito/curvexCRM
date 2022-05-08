33a3ced1f9e5256afb032fe58420c4e876c2d412
Bantosito <72502994+Bantosito@users.noreply.github.com>
2022-05-08 16:12:28 +0200

    Coments and Photo sending abbility added

--- CurvexCRM.py
+++ CurvexCRM.py
@@ -1,68 +1,99 @@
 import smtplib, ssl, pandas, os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+##########
+#Funtion that clears
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-port = 465  # For SSL
+port = 465  # Required For SSL
 
-
+# strings used to establish data required for proper code execution
 sender_email = "keathrowaway+receiver@gmail.com"
 usersFile = "curvexUsers.csv"
-link = "zamknijmor.de"
 
-
+#Funciton that sends an email based on users performance
 def sendemail(user,performance):
     password = input("Type your password and press enter: ")
     context = ssl.create_default_context() # Create a secure SSL context for security reasons
     receiver_email = users.iloc[user]["email"]
     currentUser = users.iloc[user]["user_name"]
+
+    msg = MIMEMultipart()
+    
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
     if performance == "great":
-        message = ( 
-            f"Subject: Hey {currentUser}!  Here is your monthly summary."
-            f"\n"
-            f"We are impressed with your resaults. "
-            f"You are doing great!"
+            msg['Subject'] =   f"Subject: Hey {currentUser}!  Here is your monthly summary."
+            message = MIMEText(
+            f"Hello {currentUser}.\nWe hope you are doing great\n"
+            f"We are impressed with your results.\n"
+            f"You are doing great!\n"
+            f"Have a look at the summary in attachment.\n"
+            f"Keep on going!\n\n"
             f"Thank you for being with us!\n"
-            f"Here is a link for your summary\n"
-            f"{link}\n"
-            f"Keep on going!\n"
             f"Curvex Team"
-        )
+            )
+            with open("yourSummary.png", 'rb') as f:
+                img_data = f.read()
+            msg.attach(message)
+            image = MIMEImage(img_data, name=os.path.basename("yourSummary.png"))
+            msg.attach(image)
+
+
     elif performance == "ok":
-        message = ( 
-            f"Subject: Hey {currentUser}!"
+            msg['Subject'] =   f"Subject: Hey {currentUser}!  Here is your monthly summary."
+            message = MIMEText(
+            f"Hello {currentUser}.\nWe hope you are doing great\n"
             f"Here is your monthly summary.\n"
             f"You are on a way to a great performance!\n"
+            f"Check out your activity summary in attachment\n"
+            f"You can also try these programs to boost your performance:....!\n\n"
             f"Thank you for being with us!\n"
-            f"Here is a link for your summary\n"
-            f"{link}\n"
-            f"How about trying these programs to boost your performance:....!\n"
             f"Curvex Team"
-        )    
+            )
+            with open("Summary.png", 'rb') as f:
+                img_data = f.read()
+            msg.attach(message)
+            image = MIMEImage(img_data, name=os.path.basename("Summary.png"))
+            msg.attach(image)
     else:
-        message = ( 
-            f"Subject: Hey {currentUser}!"
-            f"Here is your monthly summary.\n"
-            f"We can see that you weren't doing the best lately\n"
-            f"Here is a link for your summary\n"
-            f"{link}\n"
-            f"You can try these programs:....!\n"
+            msg['Subject'] =   f"Subject: Hey {currentUser}!  Here is your monthly summary."
+            message = MIMEText(
+            f"Hello {currentUser}.\nWe hope you are doing great\n"
+            f"We can see that your sessions weren't going the best lately\n"
+            f"Have a look at your monthly summary in attachments\n"
+            f"Try these meditation programs and see how good they work for you!\n\n"
+            f"Thank you for being with us!\n"
             f"Curvex Team"
-        )
+            )
+            with open("chart.png", 'rb') as f:
+                img_data = f.read()
+            msg.attach(message)
+            image = MIMEImage(img_data, name=os.path.basename("chart.png"))
+            msg.attach(image)
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
     print("Email Sent")
+
+########
+#Funtion used to read csv file and translate it into dataframe
 def loadUsers():
     global users
     users = pandas.read_csv (usersFile,index_col= 0)
     print(users)
 
+########
+#Funtion Used to send not active user push notification
 def sendPushNotification(user):
     print("Hey " + users.iloc[user]["user_name"]+" come back to us!")
 
+# Main Program logics
 loadUsers()
 clear()
 print("Curvex CRM mockup")
@@ -70,6 +101,7 @@ usersPrint = users.drop("email",axis = 1).rename(columns={'user_name':'Name'})
 print(usersPrint.to_string())
 
 choice = int(input("Choose an user:")) - 1
+#If statement goes through precreated logic (as seen on whiteboard model)
 if users.iloc[choice]["subscription"] == "no":
     print("Sorry this user is not a Subscriber")
 else:
